fb7143c55ff99d63537e2e3cebcd1be4f511bf07
hans <hsuchinhaha@gmail.com>
2018-07-18 17:54:23 +0800

    Update tomcat to 8.5.32

--- scripts/check_pk_health.sh
+++ scripts/check_pk_health.sh
@@ -6,7 +6,7 @@ CATALINA_HOME=/usr/share/tomcat-codedeploy
 
 # Tomcat version
 TOMCAT_VER=8
-TOMCAT_VERSION=8.5.31
+TOMCAT_VERSION=8.5.32
 # Tar file name
 TOMCAT_CORE_TAR_FILENAME="apache-tomcat-$TOMCAT_VERSION.tar.gz"
 # Download URL for Tomcat core
